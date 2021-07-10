30ae75edd5837b787734b88c784314da3c0fe2d6
stonerains <indawoom@naver.com>
2021-07-10 20:21:38 +0900

    first commit

---
+++ gitpull.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+export LD_LIBRARY_PATH=/data/data/com.termux/files/usr/lib
+export HOME=/data/data/com.termux/files/home
+export PATH=/usr/local/bin:/data/data/com.termux/files/usr/bin:/data/data/com.termux/files/usr/sbin:/data/data/com.termux/files/usr/bin/applets:/bin:/sbin:/vendor/bin:/system/sbin:/system/bin:/system/xbin:/data/data/com.termux/files/usr/bin/python
+export PYTHONPATH=/data/openpilot
+
+cd /data/openpilot
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+HASH=$(git rev-parse HEAD)
+/data/data/com.termux/files/usr/bin/git fetch
+REMOTE_HASH=$(git rev-parse --verify origin/$BRANCH)
+cd /data/openpilot; GIT_SSL_NO_VERIFY=0 => SSL
+/data/data/com.termux/files/usr/bin/git pull
+
+if [ "$HASH" != "$REMOTE_HASH" ]; then
+  sleep 3s
+  reboot
+fi
